631455dc7070e8c6673603b9efd20de22aede79a
dgp52 <deeppatel19940@gmail.com>
2018-10-09 22:54:19 -0400

    Main class added - testing jitpack with a demo proj

---
+++ bindjson2viewlib/src/main/java/com/dgp52/bindjson2viewlib/BindJson2View.java
@@ -0,0 +1,10 @@
+package com.dgp52.bindjson2viewlib;
+
+import android.util.Log;
+
+public class BindJson2View {
+
+    public BindJson2View() {
+        Log.i("log-cat", "BindJson2View");
+    }
+}
