afaa34c96a81ad8631b9364f6b1f97cfea9ce48b
dgp52 <deeppatel19940@gmail.com>
2018-10-11 23:25:03 -0400

    Added Java version 1.8 to use java 8 language feature - lambda

--- bindjson2viewlib/src/main/java/com/dgp52/bindjson2viewlib/wrappers/URlWrapper.java
+++ bindjson2viewlib/src/main/java/com/dgp52/bindjson2viewlib/wrappers/URlWrapper.java
@@ -1,21 +1,20 @@
 package com.dgp52.bindjson2viewlib.wrappers;
 
-import android.util.Log;
-
 import com.dgp52.bindjson2viewlib.logexception.ServiceException;
 
 import java.net.URL;
 
-public class URlWrapper implements Runnable {
+public class URlWrapper {
     private URL url;
 
     public URlWrapper(URL url) {
         this.url = url;
     }
 
-    @Override
-    public void run() {
-        ServiceException.logI(url.getHost());
-        ServiceException.logI("URLWrapper Run method");
+    public void start() {
+        new Thread(()-> {
+                ServiceException.logI(url.getHost());
+                ServiceException.logI("URLWrapper Run method");
+            }).start();
     }
 }
